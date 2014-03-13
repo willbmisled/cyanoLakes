24d055f80662099accf66714e2f282ef4f1a15fb
Jeff Hollister <jeff.w.hollister@gmail.com>
2014-03-13 13:13:26 -0400

    runCondProb changes

--- jeff/runCondProb.R
+++ jeff/runCondProb.R
@@ -1,2 +1,13 @@
+setwd("D:/DATA/SSWRNexusCyano/cyanoLakes/jeff")
+require(condprob2)
+source("getBioVolCatData.R")
 cutoff<-quantile(bioV$sumLbioV, na.rm=T)[4]
-bioVcpa<-condprob(bioV$CHLA,bioV$sumLbioV,cutoff,"gt","gte", T)
+binCutoff<-as.numeric(bioV$sumLbioV>=cutoff)
+minss<-cpaMinSamp(binCutoff,10,20,0.01)
+bioVcpa<-condprob(bioV$CHLA,bioV$sumLbioV,cutoff,"gt","gte", T,R=1000,
+                  minSamp=minss)
+plot.condprob<-function(condprobObj,...){
+  plot(condprobObj[,1],condprobObj[,2],...)
+  lines(condprobObj[,1],condprobObj[,4])
+  lines(condprobObj[,1],condprobObj[,5])
+}
