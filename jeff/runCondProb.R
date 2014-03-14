5ddd1cf78e4571e7205b5f134252336a8304b0b9
Jeff Hollister <jeff.w.hollister@gmail.com>
2014-03-14 16:01:11 -0400

    update on runCondProb

--- jeff/runCondProb.R
+++ jeff/runCondProb.R
@@ -3,11 +3,13 @@ require(condprob2)
 source("getBioVolCatData.R")
 cutoff<-quantile(bioV$sumLbioV, na.rm=T)[4]
 binCutoff<-as.numeric(bioV$sumLbioV>=cutoff)
-minss<-cpaMinSamp(binCutoff,10,20,0.01)
-bioVcpa<-condprob(bioV$CHLA,bioV$sumLbioV,cutoff,"gt","gte", T,R=1000,
-                  minSamp=minss)
-plot.condprob<-function(condprobObj,...){
-  plot(condprobObj[,1],condprobObj[,2],...)
+binCutoff<-binCutoff[!is.na(binCutoff)]
+bioVcpa<-condprob(bioV$CHLA,bioV$sumLbioV,cutoff,"gt","gte", T,R=1000)
+plot.condprob<-function(condprobObj,minss,...){
+  maxX<-max(condprobObj[,1][1:(length(condprobObj[,1])-minss)])
+  plot(condprobObj[,1],condprobObj[,2],ylim=c(0,1),xlim=c(0,maxX)
+       ,...)
   lines(condprobObj[,1],condprobObj[,4])
   lines(condprobObj[,1],condprobObj[,5])
 }
+plot.condprob(bioVcpa,100)
